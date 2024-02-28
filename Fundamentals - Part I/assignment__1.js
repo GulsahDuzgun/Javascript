2cb692580abf483c31d70dc54710781783d5948f
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-02-28 11:30:56 +0300

    assigment 1

---
+++ Fundamentals - Part I/assignment__1.js
@@ -0,0 +1,7 @@
+let country = "Turkey";
+let continent = "Southeastern Europe and Southwestern Asia";
+let population = " 85.3 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
