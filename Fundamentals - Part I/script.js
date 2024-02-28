3ea6b3dd7d7420a127f103db09a392d63368d6dd
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-02-28 10:53:33 +0300

    values and variables
    If value  is constant , it should be written uppercase. And variables can consist of numbers, characters, underscore and dollar sign.

--- Fundamentals - Part I/script.js
+++ Fundamentals - Part I/script.js
@@ -1,4 +1,13 @@
+//Values and Variables
+/*
+let firstname = "Matilda";
+let person = "jonas";
+let PI = "3.14";
+*/
+
 // Linking  a JS File
+/*
 let js = "awesome";
 if (js === "awesome");
 console.log(js);
+*/
