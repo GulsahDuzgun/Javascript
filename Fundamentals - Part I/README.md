beae97cce0177b0fee10f77b5f391042178d6945
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-02-28 12:10:58 +0300

    data types
    JS has two types of value type. Object and Primitive. Primitive consist of String, Number, Boolean, Undefined, Null, Symbol and BigInt

---
+++ Fundamentals - Part I/README.md
@@ -0,0 +1,8 @@
+### What did I learn while coding this project
+
+> #
+>
+> - _the 7 primitive data types in Javascript (Number, String, Boolean, Undefined, Null, Symbol, BigInt),_
+> - _Javascript has dynamic typing. We dont have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically,_
+>
+> #
--- Fundamentals - Part I/script.js
+++ Fundamentals - Part I/script.js
@@ -1,13 +1,29 @@
-//Values and Variables
-/*
-let firstname = "Matilda";
-let person = "jonas";
-let PI = "3.14";
-*/
+//-----Data Types-----
+let jsIsFun = "Yes";
+console.log(typeof jsIsFun);
 
-// Linking  a JS File
+jsIsFun = false;
+console.log(typeof jsIsFun);
+
+let react = null;
+console.log(typeof react);
+
+nodeJs = undefined;
+console.log(typeof nodeJs);
+
+nextJS = 9007199254740991n;
+console.log(typeof nextJS);
+
+//-----Values and Variables-----
 /*
-let js = "awesome";
-if (js === "awesome");
-console.log(js);
-*/
+  let firstname = "Matilda";
+  let person = "jonas";
+  let PI = "3.14";
+  */
+
+// -----Linking  a JS File-----
+/*
+  let js = "awesome";
+  if (js === "awesome");
+  console.log(js);
+  */
