2a19962545f5c6ed03f933e0951460ac80b03bc3
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-02-29 10:28:36 +0300

    String and Template Literals

--- Fundamentals - Part I/README.md
+++ Fundamentals - Part I/README.md
@@ -6,5 +6,7 @@
 > - _the 7 primitive data types in Javascript (Number, String, Boolean, Undefined, Null, Symbol, BigInt),_
 > - _Javascript has dynamic typing. We dont have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically,_
 > - _let could be mutate but const couldn't,_
+> - _operator precedence in Javascript_
+> - _difference between String and Template Literals,_
 >
 > #
--- Fundamentals - Part I/script.js
+++ Fundamentals - Part I/script.js
@@ -1,3 +1,33 @@
+//String and Template Literals
+
+const city = "İstanbul";
+const age = 24;
+const firstName = "Gülşah";
+
+const text1 =
+  "I'm " +
+  firstName +
+  " and I'm " +
+  age +
+  ". I have been living in " +
+  city +
+  " for 2 years.";
+
+const tempLiteral = `I'm ${firstName} and I'm ${age}. I have been living in ${city} for 2 years.`;
+
+console.log(text1);
+console.log(tempLiteral);
+
+const spreadString = "Hello, \n  Believe in yourself!";
+const spreadString2 = `Hello,
+ Believe in yourself!`;
+
+console.log(spreadString);
+console.log(spreadString2);
+
+/*
+//-----LET, CONST AND VAR-----
+
 let age = 24;
 console.log(age);
 
@@ -7,6 +37,7 @@ console.log(age);
 const birthYear = 2000;
 console.log(birthYear);
 //birthYear = 1999;  couldn't be mutate
+*/
 
 /*
 //-----Data Types-----
