f4a7d6f826f2eacb470b7cbafb2e552313269100
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-05 14:29:53 +0300

    Selecting and Manipulating Elements
    Document Object Model is structed representation of html documents. Allow Js to access html elements ans styles to manipulate them. DOM is connection point between js and html. Also like document.querySelector document is an special object as entry point to the DOM. DOM is not part of JS. It's part of Web APIs also was written with js.
    For Input element's content we use value, textContent for other elemet's

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,6 +1,22 @@
 'use strict';
+//----- Selecting and Manipulating Elements -----
 
+const number = document.querySelector('.number');
+number.textContent = 24;
+console.log(number.textContent);
+
+document.querySelector('.message').textContent = 'Correct number 🎉';
+
+const inputNum = (document.querySelector('.left .guess').value = 25);
+const inputNum2 = (document.querySelector('.left .guess').textContent = 30);
+
+console.log(inputNum);
+console.log(inputNum2);
+
+/*
+//----- DOM -----
 const btn = document.querySelector('.btn.check');
 const btnText = document.querySelector('.btn.check').textContent;
 console.log(btn);
 console.log(btnText);
+*/
