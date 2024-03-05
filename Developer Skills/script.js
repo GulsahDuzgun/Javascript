39da2c927d9edcd15bd08809a3c5d84f399b3d8c
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-05 10:27:24 +0300

    Debugging with the Console and Breakpoints

--- Developer Skills/script.js
+++ Developer Skills/script.js
@@ -1,3 +1,50 @@
+'use strict';
+
+//Debugging with the Console and Breakpoints
+
+const temperatures = [3, 4, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcAmplitudeBug = function (arr1, arr2) {
+  const tempArr = arr1.concat(arr2);
+  let tempMax = 0,
+    tempMin = 0;
+
+  for (let i = 0; i <= tempArr.length; i++) {
+    if (typeof tempArr[i] !== 'number') continue;
+
+    debugger;
+    if (tempMax < tempArr[i]) {
+      tempMax = tempArr[i];
+    }
+
+    if (tempMin > tempArr[i]) {
+      tempMin = tempArr[i];
+    }
+  }
+
+  console.log(tempMax);
+  console.log(tempMin);
+  return tempMax - tempMin;
+};
+
+console.log(calcAmplitudeBug(temperatures, [18, 24, 2, 8]));
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // value: Number(prompt('Degrees celsius')),
+    value: 10,
+  };
+
+  console.log(measurement);
+  console.table(measurement);
+  return measurement.value + 273;
+};
+
+console.log(measureKelvin());
+
+/*
 //Solve Poblem
 
 'use strict';
@@ -51,6 +98,7 @@ console.log('----- One Array -----');
 console.log(calcAmplitude(temperatures));
 console.log('----- Two Array -----');
 console.log(calcAmplitude2(temperatures, [18, 24, -2, -8]));
+*/
 
 /*
 //Setting up Prettier and VS Code
