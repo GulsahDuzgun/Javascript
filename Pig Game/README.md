db35ed716d9651961e25c1ca50cfcb7c19114c56
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-06 10:48:59 +0300

    Rolling the Dice

--- Pig Game/README.md
+++ Pig Game/README.md
@@ -3,5 +3,6 @@
 > #
 >
 > - _document.getElementById() ,_
+> - _how to set img src by using template literals ,_
 >
 > #
--- Pig Game/script.js
+++ Pig Game/script.js
@@ -3,7 +3,29 @@
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
 const dice = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
+let tempScore = 0;
 
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const randNum = Math.trunc(Math.random() * 6) + 1;
+  console.log(randNum);
+  dice.src = `./dice-${randNum}.png`;
+  dice.classList.remove('hidden');
+
+  if (randNum !== 1) {
+    tempScore += randNum;
+  } else {
+    tempScore = 0;
+  }
+
+  current0.textContent = tempScore;
+});
