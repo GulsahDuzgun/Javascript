d191affe9aaabd4d8082c0b42f2d40c5f0fae145
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-06 11:52:29 +0300

    Switching the Active Player

--- Pig Game/README.md
+++ Pig Game/README.md
@@ -4,5 +4,6 @@
 >
 > - _document.getElementById() ,_
 > - _how to set img src by using template literals ,_
+> - _how to activete-deactivate class with toggle method ,_
 >
 > #
--- Pig Game/script.js
+++ Pig Game/script.js
@@ -3,13 +3,18 @@
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
 const dice = document.querySelector('.dice');
+
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
+
 const current0 = document.getElementById('current--0');
 const current1 = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
 let tempScore = 0;
+let activePlayer = 0;
 
 score0.textContent = 0;
 score1.textContent = 0;
@@ -17,15 +22,17 @@ dice.classList.add('hidden');
 
 btnRoll.addEventListener('click', function () {
   const randNum = Math.trunc(Math.random() * 6) + 1;
-  console.log(randNum);
   dice.src = `./dice-${randNum}.png`;
   dice.classList.remove('hidden');
 
   if (randNum !== 1) {
     tempScore += randNum;
+    document.getElementById(`current--${activePlayer}`).textContent = tempScore;
   } else {
     tempScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = tempScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
-
-  current0.textContent = tempScore;
 });
