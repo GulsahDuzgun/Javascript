32054d8c1b1b11c46fc67d3d21a74df22428e542
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-09 14:44:45 +0300

    Scoping in Practice  :tiger:
    let and const are block scope, var is function scope.you can access var valuable inside from the closest function.

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const firstName = 'Gülşah';
+calcAge(2000);
+
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+
+  const printAge = function () {
+    const output = `${firstName}, you are ${age}.`;
+    console.log(output);
+
+    if (birthYear <= 2012 && birthYear >= 1996) {
+      var zFlag = true;
+      const zGen = `Hi ${firstName} , You are Zoomers`;
+      console.log(zGen);
+    }
+    console.log(zFlag);
+    // console.log(zGen); ReferenceError
+  };
+  // console.log(zFlag); ReferenceError
+
+  printAge();
+}
