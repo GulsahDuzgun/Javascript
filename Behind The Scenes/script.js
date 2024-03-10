cc11e451381360aaab093ab32e987cad688885d8
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-10 21:31:10 +0300

    Scoping in Practice -- Part 3
    If the variable was created in function scope with same name an another value had created before in parents scope, that is not a problem.  Actually this is how it works on same parameter name in functions. Because they have their own execution context. So the variable is completely created as a new variable.

--- Behind The Scenes/script.js
+++ Behind The Scenes/script.js
@@ -7,24 +7,30 @@ function calcAge(birthYear) {
   const age = 2025 - birthYear;
 
   const printAge = function () {
-    const output = `${firstName}, you are ${age}.`;
-    console.log(output);
+    const firstName = 'Esra';
+    let output = `${firstName}, you are ${age}.`;
 
     if (birthYear <= 2012 && birthYear >= 1996) {
       var zFlag = true;
       const zGen = `Hi ${firstName} , You are Zoomers`;
+      output = `You are ${age} years old.`;
+
       console.log(zGen);
+      console.log(output);
 
       function add(a, b) {
         return a + b;
       }
       console.log(add(4, 5));
     }
+
+    console.log(output);
     console.log(zFlag);
-    console.log(add(4, 5));
+    // console.log(add(4, 5)); ReferenceError
     // console.log(zGen); ReferenceError
   };
   // console.log(zFlag); ReferenceError
 
   printAge();
+  console.log(firstName);
 }
