a8a129852fdb5e01a34f49591403cf403105d2cf
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-14 00:22:24 +0300

    Destructuring Arrays -- 1.3

--- Assigments for The Complete Course/dataStructuresAndOperators.js
+++ Assigments for The Complete Course/dataStructuresAndOperators.js
@@ -232,3 +232,14 @@ console.log(firstBook, secondBook);
 
 const [, , thirdBook] = books;
 console.log(thirdBook);
+
+/*1.3
+Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.*/
+
+const ratings = [
+  ["rating", 4.19],
+  ["ratingsCount", 144584],
+];
+
+const [[, rating], [, ratingsCount]] = ratings;
+console.log(rating, ratingsCount);
