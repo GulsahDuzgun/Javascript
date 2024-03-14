a273524a8d4dbcdbe229d27c70258e9463445a8c
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-14 17:32:22 +0300

    Rest Pattern and Parameters 4.1

--- Assigments for The Complete Course/dataStructuresAndOperators.js
+++ Assigments for The Complete Course/dataStructuresAndOperators.js
@@ -223,18 +223,28 @@ const books = [
   },
 ];
 
-//The Spread Operator
+//Rest Pattern and Parameters
+
+/*
+4.1
+Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).*/
+
+const [mainKeyword, ...rest] = books[0].keywords;
+console.log(mainKeyword, rest);
+
+//The Spread Operator -- unpacking
 
 /*3.1
 Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
 
-Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).*/
+Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
 
 const bookAuthors = [...books[0].author, ...books[1].author];
 console.log(bookAuthors);
+*/
 
 /*3.2
-Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.*/
+Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
 
 const spellWord = function (arg) {
   const a = [...arg];
@@ -252,6 +262,7 @@ const spellWord = function (arg) {
 };
 
 spellWord('JavaScript');
+*/
 
 //----- Destructuring Objects ------
 /*2.1
