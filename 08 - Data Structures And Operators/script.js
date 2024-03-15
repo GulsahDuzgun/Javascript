9b25454b01ee802e561c8283685eea0571fb70d4
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-15 15:20:38 +0300

    Enhanced Object Literals

--- 08 - Data Structures And Operators/script.js
+++ 08 - Data Structures And Operators/script.js
@@ -1,26 +1,33 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+console.log(openingHours);
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
+  // openingHours: openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
   getOrder: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -39,11 +46,14 @@ const restaurant = {
     console.log(`Here the ingredients in pasta: 
     ${ing1}, ${ing2}, ${ing3}`);
   },
-  orderPizza: function (mainIngredients, ...otherIngredients) {
+  orderPizza(mainIngredients, ...otherIngredients) {
     //console.log(typeof otherIngredients); object
     console.log(mainIngredients, otherIngredients);
   },
 };
+
+console.log(restaurant.openingHours);
+
 ///////////////////////////////////////
 // Looping Arrays:The for-of Loop
 
