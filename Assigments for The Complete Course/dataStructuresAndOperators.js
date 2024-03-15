c8e5bd18dea14c7018dac331be9bb5b168291e49
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-15 10:37:31 +0300

    Logical Assignments Operators --assigment

--- Assigments for The Complete Course/dataStructuresAndOperators.js
+++ Assigments for The Complete Course/dataStructuresAndOperators.js
@@ -223,14 +223,32 @@ const books = [
   },
 ];
 
+//Logical Assignments Operators
+/*
+7.1
+Some of the book objects from the books array are missing the edition property. Loop over the books array, and assign this property with a number 1 (if it doesn't already exist). Use logical assignment operators.*/
+
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ??= 1;
+}
+
+/*7.2
+Some of the book objects from the books array have the highlighted property, which by default is set to true. Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.*/
+for (let i = 0; i < books.length; i++) {
+  books[i].highlighted &&= books[i].thirdParty.goodreads.rating >= 4.2;
+  // books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2);
+  console.log(books[i].highlighted);
+}
+
 //The Nullish Coalescing(??)
 /*6.1
-There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.*/
+There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
 
 for (let i = 0; i < books.length; i++) {
   books[i].onlineContent ??
     console.log(`${books[i].title}" provides no data about its online content`);
 }
+*/
 
 //Short Circuiting
 /*5.1
