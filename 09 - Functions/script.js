3513bc8d473f77f92555fdc1a1ab85baf8e91acd
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-18 16:39:18 +0300

    Functions Accepting Callback Functions

--- 09 - Functions/script.js
+++ 09 - Functions/script.js
@@ -35,6 +35,42 @@ checkIn(me, flight);
 */
 
 ///////////////////////////////////////
+// Functions Accepting Callback Functions
+/* */
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// console.log(upperFirstWord('Javascript is the best'));
+// console.log(oneWord('Javascript is the best'));
+
+//Higher-Order Functions
+const transformer = function (str, fn) {
+  console.log(str);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by:${fn.name}`);
+};
+
+transformer('Javascript is the best', upperFirstWord);
+transformer('Javascript is the best', oneWord);
+
+//JS uses callbacks functions all the time.
+
+const high5 = function () {
+  console.log('🖐');
+};
+
+document.body.addEventListener('click', high5);
+
+['a', 2, 'abc'].forEach(high5);
+
+///////////////////////////////////////
 // Default Parameters
 /*
 const bookings = [];
