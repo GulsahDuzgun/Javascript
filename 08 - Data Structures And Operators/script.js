ff87879fca63ca524923f478a01f296b47760b2d
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-18 14:13:16 +0300

    String Methods Practice

--- 08 - Data Structures And Operators/script.js
+++ 08 - Data Structures And Operators/script.js
@@ -51,6 +51,35 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsArr = flights.trim().split('+');
+
+const getThreeLetter = strNum => {
+  return strNum.slice(0, 3).toUpperCase();
+};
+
+for (const flightInfo of flightsArr) {
+  let [type, from, to, time] = flightInfo.split(';');
+  type = type.replaceAll('_', ' ').trim();
+  type = (type.includes('Delayed') ? '🔴 ' : '') + type;
+
+  from = getThreeLetter(from);
+  to = getThreeLetter(to);
+  time = time.replace(':', 'h');
+
+  const res = `${type} from ${from} to ${to} (${time})`;
+  console.log(res.padStart(50, ' '));
+}
+///////////////////////////////////////
 // Coding Challenge #4
 
 /* 
@@ -79,7 +108,7 @@ HINT 4: This challenge is difficult on purpose, so start watching the solution i
 
 Afterwards, test with your own test data!
 
-GOOD LUCK 😀*/
+GOOD LUCK 😀
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -100,7 +129,7 @@ btn.addEventListener('click', function () {
     }
   }
 });
-
+*/
 ///////////////////////////////////////
 // Working With Strings- Part 3
 /*
