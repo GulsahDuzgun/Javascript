c3821bcde4c6e95f251bc49bb871ab52ed5c2cd0
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-18 15:27:35 +0300

    How Passing Arguments Works: Value vs. Reference

--- 09 - Functions/script.js
+++ 09 - Functions/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
+const flight = 23456787654;
+
+const me = {
+  firstName: 'Gülşah',
+  passport: 345678909876,
+};
+
+const checkIn = function (passenger, flightNum) {
+  passenger.firstName = 'Mrs. ' + passenger.firstName;
+
+  if (passenger.passport === 345678909876) {
+    flightNum = 11111111111;
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(me, flight);
+// console.log(me, flight);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000);
+};
+
+newPassport(me);
+console.log(me, flight);
+checkIn(me, flight);
+
+///////////////////////////////////////
+// Default Parameters
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -24,3 +56,5 @@ createBooking('ED12', 127);
 createBooking(undefined, 58);
 createBooking('OSL27', undefined, 73);
 createBooking('OSL27', 92);
+
+*/
