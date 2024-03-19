5c8d406f1b67596e13274bcd1a2e5f668fe00c5e
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-19 17:41:26 +0300

    Looping Arrays: forEach
    arr.forEach(function(){
    //code
    })
    forEach is a high-order function and it has callback function to trigger when loop over the array

--- 10 - Arrays/script.js
+++ 10 - Arrays/script.js
@@ -1,7 +1,32 @@
 'use strict';
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+/* */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}:You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('------ FOREACH ------');
+//callback --> function(200,0 , [])
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}:You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}:You withdrew ${Math.abs(movement)}`);
+  }
+});
+
 ///////////////////////////////////////
 // The new at Method
-/* */
+/* 
 
 const arr = [27, 12, 2000];
 console.log(arr[0]);
@@ -15,6 +40,9 @@ console.log(arr.at(-2));
 
 console.log('gulsah'.at(0));
 console.log('gulsah'.at(-1));
+
+*/
+
 ///////////////////////////////////////
 // Simple Array Methods
 /*
