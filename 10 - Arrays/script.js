58f78863ebba659187c529849b8ef3d739835da7
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-20 12:14:09 +0300

    Coding Challenge #2

--- 10 - Arrays/script.js
+++ 10 - Arrays/script.js
@@ -84,8 +84,60 @@ calcDisplayBalance(account1.movements);
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 ///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function (ages) {
+  // const calcAges = ages.map(function (el, indx, arr) {
+  //   return el <= 2 ? el * 2 : 16 + 4 * el;
+  // });
+  // console.log(calcAges);
+
+  // const calcAdult = calcAges.filter(function (el, indx, arr) {
+  //   return el >= 18;
+  // });
+  // console.log(calcAdult);
+
+  // const average =
+  //   calcAdult.reduce(function (acc, el, indx, arr) {
+  //     return acc + el;
+  //   }, 0) / calcAdult.length;
+
+  // console.log(average);
+  // return average;
+
+  const calcArr = ages
+    .map(el => (el <= 2 ? el * 2 : 16 + 4 * el))
+    .filter(e => e >= 18);
+
+  // const average = calcArr.reduce((acc, el) => acc + el, 0) / calcArr.length;
+  const average = calcArr.reduce((acc, el, i, arr) => acc + el / arr.length, 0);
+  // 2+3/2 => 2/2 + 3/2
+
+  return average;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+///////////////////////////////////////
 // The reduce Method
-/* */
+/* 
 
 //Accumulator -> SNOWBAL
 const totalMov = movements.reduce(function (acc, element, index, arr) {
@@ -105,6 +157,7 @@ const maxVal = movements.reduce(function (acc, val) {
 }, movements[0]);
 
 console.log(maxVal);
+*/
 
 ///////////////////////////////////////
 // The filter Method
