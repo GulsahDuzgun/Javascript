e82e33bbd9946a54d3cf3cab3f08e373e3ada2d0
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-30 20:41:17 +0300

    Encapsulation: Protected Properties and Methods

--- 13 - Object Oriented Programming/script.js
+++ 13 - Object Oriented Programming/script.js
@@ -8,26 +8,25 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
     this.locale = navigator.language;
-    console.log(`Thanks for opening an account, ${owner}`);
+    this._pin = pin;
+    this._movements = [];
   }
 
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
-    this.movements.push(-val);
+    this._movements.push(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log('Loan approved');
     }
@@ -37,7 +36,7 @@ class Account {
 const acc1 = new Account('Gülşah', 'EUR', 111111);
 acc1.deposit(400);
 acc1.withdraw(300);
-console.log(acc1.approveLoan());
+console.log(acc1._approveLoan());
 acc1.requestLoan(200);
 console.log(acc1);
 
