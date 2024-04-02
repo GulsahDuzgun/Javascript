d5900a18893d97298dafb7feafc71eadc39edf88
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-04-02 15:17:18 +0300

    Refactoring for Project Architecture

--- 14 - Mapty/script.js
+++ 14 - Mapty/script.js
@@ -12,80 +12,81 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 let globalE, globalMap;
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude, longitude } = position.coords;
-    console.log(latitude, longitude);
-    console.log(
-      `https://www.google.com/maps/@${latitude},${longitude},12z?entry=ttu`
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert('Could not get your position ');
+      }
     );
+  }
 
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
-    console.log(L.map);
-    const map = L.map('map').setView(coords, 13);
+
+    // console.log(
+    //   `https://www.google.com/maps/@${latitude},${longitude},12z?entry=ttu`
+    // );
+
+    this.#map = L.map('map').setView(coords, 13);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    globalMap = map;
-    map.on('click', function (mapEvent) {
-      globalE = mapEvent;
+    this.#map.on('click', this._showForm.bind(this));
 
-      form.classList.remove('hidden');
-      inputDistance.focus();
-      // const { lat, lng } = mapEvent.latlng;
-      // L.marker([lat, lng])
-      //   .addTo(map)
-      //   .bindPopup(
-      //     L.popup({
-      //       maxWidth: 250,
-      //       minWidth: 100,
-      //       autoClose: false,
-      //       closeOnClick: false,
-      //       clasName: 'cycling-popup',
-      //     })
-      //   )
-      //   .setPopupContent('Hi bro')
-      //   .openPopup();
-    });
-
-    L.marker(coords).addTo(map).bindPopup('Hello').openPopup();
-  },
-  function () {
-    alert('Could not get your position ');
+    L.marker(coords).addTo(this.#map).bindPopup('Hello').openPopup();
   }
-);
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+  _newWorkout(e) {
+    e.preventDefault();
 
-  inputCadence.value =
-    inputDistance.value =
-    inputDuration.value =
-    inputElevation.value =
-      '';
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
 
-  const { lat, lng } = globalE.latlng;
-  L.marker([lat, lng])
-    .addTo(globalMap)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        clasName: 'cycling-popup',
-      })
-    )
-    .setPopupContent('Hi bro')
-    .openPopup();
-});
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          clasName: 'cycling-popup',
+        })
+      )
+      .setPopupContent('Hi bro')
+      .openPopup();
+  }
 
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+}
+
+const app = new App();
