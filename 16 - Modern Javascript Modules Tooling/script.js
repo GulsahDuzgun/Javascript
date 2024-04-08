8f3f83fee62d4c3db5d32d7c62f6dad55bbe4d47
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-04-08 11:56:19 +0300

    Top-Level await
    Using top-level await in modules (out of asyb-nc function) will block the entire module.
    
     An async function always returns a promise not data itself because by the time we are running the get data , actually async data has not arrived yet

--- 16 - Modern Javascript Modules Tooling/script.js
+++ 16 - Modern Javascript Modules Tooling/script.js
@@ -1,8 +1,28 @@
 import add, { age, ct } from './shoppingCard.js';
 
 ///////////////////////////////////////
+//
+/*Top-Level await */
+
+const getLastPost = async function () {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/posts`).then(
+    res => res.json()
+  );
+
+  const { title, body: comment } = data.at(-1);
+  return { title, comment };
+};
+
+const res = getLastPost(); //Promise
+const res2 = getLastPost().then(res => console.log(res));
+const res3 = await getLastPost();
+console.log(res);
+console.log(res2);
+console.log(res3);
+
+///////////////////////////////////////
 // Exporting and Importing in ES6 Modules - Part II
-/* */
+/* 
 
 // ShoppingCart.addToCart('grapes', 3);
 // console.log(ShoppingCart.age);
@@ -12,6 +32,7 @@ add('apple', 2);
 
 console.log(age);
 console.log(ct);
+*/
 
 ///////////////////////////////////////
 // Exporting and Importing in ES6 Modules
--- 16 - Modern Javascript Modules Tooling/shoppingCard.js
+++ 16 - Modern Javascript Modules Tooling/shoppingCard.js
@@ -1,10 +1,16 @@
 console.log('Export Module');
 
+console.log('Fetch Start');
+const data = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+const jsonData = await data.json();
+console.log('Fetch Finish');
+
+console.log(jsonData);
+
 export const age = 24;
 
 const shippingCost = 10;
 const cart = [];
-
 console.log(cart);
 
 export { shippingCost, cart as ct };
