82c16af6b98cbb0b45c0d3b24f922a3ce1c60470
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-04-09 16:44:49 +0300

    Loading a Recipe from API

--- 17 - Forkify/dist/index.e37f48ea.js
+++ 17 - Forkify/dist/index.e37f48ea.js
@@ -591,8 +591,30 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+const getData = async function() {
+    try {
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${res.status}:  ${data.message}`);
+        let { recipe } = data.data;
+        console.log(recipe);
+        recipe = {
+            id: recipe.id,
+            serving: recipe.serving,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            ingredients: recipe.ingredients,
+            sourceUrl: recipe.source_url,
+            img: recipe.image_url,
+            cookingTime: recipe.cooking_time
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err.message);
+    }
+};
+getData();
 
 },{}]},["hycaY","aenu9"], "aenu9", "parcelRequire0f77")
 
--- 17 - Forkify/src/js/controller.js
+++ 17 - Forkify/src/js/controller.js
@@ -8,6 +8,33 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+const getData = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
 
-///////////////////////////////////////
+    if (!res.ok) throw new Error(`${res.status}:  ${data.message}`);
+
+    let { recipe } = data.data;
+    console.log(recipe);
+
+    recipe = {
+      id: recipe.id,
+      serving: recipe.serving,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      sourceUrl: recipe.source_url,
+      img: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+getData();
